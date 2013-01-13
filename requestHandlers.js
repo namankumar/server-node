110b70d132a2591efcb55626ed182179abba5df8
namankumar <ping@namank.com>
2013-01-13 02:48:37 -0500

    requesthandlers

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start(){
+    console.log("reqHandler - start");
+}
+
+function upload(){
+    console.log("reqHandler - upload");
+}
+
+exports.start = start;
+exports.upload = upload;
--- router.js
+++ router.js
@@ -1,5 +1,12 @@
-function router(pathname){
+function route(handle, pathname){
     console.log('routing - '+ pathname);
+    
+    if (pathname === undefined) return;
+
+    if(typeof handle[pathname] === 'function')
+	handle[pathname]();
+    else
+	console.log('handler not found - ' + pathname);
 }
 
-exports.route = router;
+exports.route = route;
